378787c45b6184902f286d810337fffd1a89b995
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-03-29 21:55:41 -0600

    feat: add project base

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Container, Stack, Typography } from '@mui/material'
+
+import './App.css'
+import { JavaScriptLogo } from './JavaScript'
+
+function App() {
+  return (
+    <main>
+      <Container maxWidth="sm">
+        <Stack direction="row" gap={2} alignItems="center" justifyContent="center">
+          <JavaScriptLogo />
+          <Typography variant="h2" component="h1">
+            <h1>JavaScript Quiz</h1>
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
+  )
+}
+
+export default App
