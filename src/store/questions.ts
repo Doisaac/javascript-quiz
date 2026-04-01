4f76f635eae4c46d86796acf6be55798d66fe02b
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-03-31 22:19:16 -0600

    feat: add question's store

---
+++ src/store/questions.ts
@@ -0,0 +1,42 @@
+import { create } from 'zustand'
+
+import type { Question } from '../types'
+
+interface State {
+  questions: Question[]
+  currentQuestion: number
+  fetchQuestion: (limitNumber: number) => Promise<void>
+  selectAnswer: (questionId: number, answerIndex: number) => void
+}
+
+export const useQuestionStore = create<State>((set, get) => ({
+  questions: [],
+  currentQuestion: 0,
+  fetchQuestion: async (limitNumber: number) => {
+    const res = await fetch('http://localhost:5173/data.json')
+    const json = await res.json()
+
+    const questions = json.sort(() => Math.random() - 0.5).slice(0, limitNumber)
+    set({ questions })
+  },
+  selectAnswer: (questionId: number, answerIndex: number) => {
+    const { questions } = get()
+    const newQuestions = structuredClone(questions)
+
+    // Find the question by its id
+    const questionIndex = newQuestions.findIndex(question => question.id === questionId)
+
+    // Get its information
+    const questionInfo = newQuestions[questionIndex]
+    const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+
+    // Mutate the question data
+    newQuestions[questionIndex] = {
+      ...questionInfo,
+      isCorrectUserAnswer,
+      userSelectedAnswer: answerIndex,
+    }
+
+    set({ questions: newQuestions })
+  },
+}))
