eba0180d31601e6ed13fab795cae7682951bc068
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-04-02 15:42:52 -0600

    feat: implement persist middleware to save the state in the local storage

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import confetti from 'canvas-confetti'
+import { persist } from 'zustand/middleware'
 
 import type { Question } from '../types'
 
@@ -12,52 +13,59 @@ interface State {
   goPreviousQuestion: () => void
 }
 
-export const useQuestionStore = create<State>((set, get) => ({
-  questions: [],
-  currentQuestion: 0,
-  fetchQuestion: async (limitNumber: number) => {
-    const res = await fetch('http://localhost:5173/data.json')
-    const json = await res.json()
+export const useQuestionStore = create<State>()(
+  persist(
+    (set, get) => ({
+      questions: [],
+      currentQuestion: 0,
+      fetchQuestion: async (limitNumber: number) => {
+        const res = await fetch('http://localhost:5173/data.json')
+        const json = await res.json()
 
-    const questions = json.sort(() => Math.random() - 0.5).slice(0, limitNumber)
-    set({ questions })
-  },
-  selectAnswer: (questionId: number, answerIndex: number) => {
-    const { questions } = get()
-    const newQuestions = structuredClone(questions)
+        const questions = json.sort(() => Math.random() - 0.5).slice(0, limitNumber)
+        set({ questions })
+      },
+      selectAnswer: (questionId: number, answerIndex: number) => {
+        const { questions } = get()
+        const newQuestions = structuredClone(questions)
 
-    // Find the question by its id
-    const questionIndex = newQuestions.findIndex(question => question.id === questionId)
+        // Find the question by its id
+        const questionIndex = newQuestions.findIndex(question => question.id === questionId)
 
-    // Get its information
-    const questionInfo = newQuestions[questionIndex]
-    const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+        // Get its information
+        const questionInfo = newQuestions[questionIndex]
+        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
 
-    if (isCorrectUserAnswer) confetti()
+        if (isCorrectUserAnswer) confetti()
 
-    // Mutate the question data
-    newQuestions[questionIndex] = {
-      ...questionInfo,
-      isCorrectUserAnswer,
-      userSelectedAnswer: answerIndex,
+        // Mutate the question data
+        newQuestions[questionIndex] = {
+          ...questionInfo,
+          isCorrectUserAnswer,
+          userSelectedAnswer: answerIndex,
+        }
+
+        set({ questions: newQuestions })
+      },
+      goNextQuestion: () => {
+        const { currentQuestion, questions } = get()
+        const nextQuestion = currentQuestion + 1
+
+        if (nextQuestion < questions.length) {
+          set({ currentQuestion: nextQuestion })
+        }
+      },
+      goPreviousQuestion: () => {
+        const { currentQuestion } = get()
+        const previousQuestion = currentQuestion - 1
+
+        if (previousQuestion >= 0) {
+          set({ currentQuestion: previousQuestion })
+        }
+      },
+    }),
+    {
+      name: 'questions',
     }
-
-    set({ questions: newQuestions })
-  },
-  goNextQuestion: () => {
-    const { currentQuestion, questions } = get()
-    const nextQuestion = currentQuestion + 1
-
-    if (nextQuestion < questions.length) {
-      set({ currentQuestion: nextQuestion })
-    }
-  },
-  goPreviousQuestion: () => {
-    const { currentQuestion } = get()
-    const previousQuestion = currentQuestion - 1
-
-    if (previousQuestion >= 0) {
-      set({ currentQuestion: previousQuestion })
-    }
-  },
-}))
+  )
+)
